f14e865102e69190b8512408b2a48aa451974d86
Alex <Onyshko>
2022-09-19 13:47:36 +0300

    firebase update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'bot-administration',
+    appId: '1:229629274887:web:c6d786ac33b1744772f3a2',
+    storageBucket: 'bot-administration.appspot.com',
+    apiKey: 'AIzaSyAv4M6vfeAUIrwHVQEkR2huImX-6cD-5jI',
+    authDomain: 'bot-administration.firebaseapp.com',
+    messagingSenderId: '229629274887',
+    measurementId: 'G-C2H0RX60CY',
+  },
   production: false
 };
 
