cbbd5b235632e591b02a5402612ee0c6761a613e
Alex <Onyshko>
2022-10-03 08:13:10 +0300

    moved root component to the components folder

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,7 @@
-import { Firestore } from '@google-cloud/firestore';
 import { onRequest } from 'firebase-functions/v2/https';
 
 import { Context, Telegraf } from 'telegraf';
 import { Update } from 'typegram';
-
-const firestore = new Firestore({
-  projectId: '1:229629274887:web:c6d786ac33b1744772f3a2',
-  timestampsInSnapshots: true
-});
 
 function configureBot(token: string): Telegraf<Context<Update>> {
   const bot: Telegraf<Context<Update>> = new Telegraf(token);
