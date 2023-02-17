1aa5f235b152003b24a1b245ece6b3b4654a10b9
Alex <Onyshko>
2023-02-17 13:28:57 +0200

    removed drag and drop

--- src/app/components/shop/products/products.component.ts
+++ src/app/components/shop/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { DragAndDropContainerComponent } from '../../../shared/components/drag-and-drop-container/drag-and-drop-container.component';
 import { ProductFallbackComponent } from './product-fallback/product-fallback.component';
 import { ProductComponent } from './product/product.component';
 import { Product } from '../../../core/model/entities/product.entity';
@@ -10,14 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [
-    CommonModule,
-    ProductComponent,
-    ProductFallbackComponent,
-    DragDropModule,
-    DragAndDropContainerComponent,
-    MatIconModule
-  ],
+  imports: [CommonModule, ProductComponent, ProductFallbackComponent, DragDropModule, MatIconModule],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
