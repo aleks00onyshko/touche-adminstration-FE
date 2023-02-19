2f864c3bc47886e278cd289f03aacec7eeff65ff
Alex <Onyshko>
2023-02-20 01:47:51 +0200

    systembar fix

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,16 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Store, StoreModule } from '@ngrx/store';
+import { map, Observable } from 'rxjs';
 import { SystembarComponent } from '../shared/components/systembar/systembar.component';
+import { AuthenticationState } from '../store/authentication/authentication.reducer';
+import { selectUser } from '../store/authentication/authentication.selectors';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, SystembarComponent],
+  imports: [CommonModule, RouterModule, SystembarComponent, StoreModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
-  title = 'bot-administration-FE';
+export class AppComponent implements OnInit {
+  public isAuthenticated$!: Observable<boolean>;
+
+  constructor(private store: Store<AuthenticationState>) {}
+
+  public ngOnInit(): void {
+    this.isAuthenticated$ = this.store.select(selectUser).pipe(map(user => !!user));
+  }
 }
