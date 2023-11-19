74b23c932404892b1b5d2574d69717cebcee9bf7
Alex Onyshko <aon@celum.com>
2023-11-19 11:14:22 +0200

    Added list of time slots

--- src/app/components/dashboard/time-slots/create-time-slot-dialog/create-time-slot-dialog.component.ts
+++ src/app/components/dashboard/time-slots/create-time-slot-dialog/create-time-slot-dialog.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import {
-  TimeSlotCardControlValue,
-  TimeSlotCardComponent,
-
-} from '../time-slot/time-slot-card.component';
+import { TimeSlotCardControlValue, TimeSlotCardComponent } from '../time-slot/time-slot-card.component';
 import * as moment from 'moment';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -21,10 +17,7 @@ export class CreateTimeSlotDialogComponent {
   public readonly defaultTimes: [number, number] = [moment().hour(), moment().minute()];
 
   public readonly timeSlotControl: FormControl<TimeSlotCardControlValue | null> =
-    new FormControl<TimeSlotCardControlValue>({
-      startTime: this.defaultTimes,
-      duration: 15
-    });
+    new FormControl<TimeSlotCardControlValue>({ startTime: this.defaultTimes, duration: 15 });
 
   constructor(private readonly matDialogRef: MatDialogRef<CreateTimeSlotDialogComponent>) {}
 
