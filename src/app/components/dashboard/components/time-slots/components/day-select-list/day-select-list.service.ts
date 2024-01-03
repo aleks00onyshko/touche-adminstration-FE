8175c17017c6ace731de2368401659c3c8981478
Alex Onyshko <aon@celum.com>
2024-01-03 14:19:51 +0200

    new changes

--- src/app/components/dashboard/components/time-slots/components/day-select-list/day-select-list.service.ts
+++ src/app/components/dashboard/components/time-slots/components/day-select-list/day-select-list.service.ts
@@ -20,22 +20,21 @@ export class DaySelectListService {
     const days: DayLabel[] = [];
 
     const addDays = (numberOfDays: number, currentDate: moment.Moment, days: DayLabel[]) => {
-      for (let i = 0; i < 30; i++) {
+      for (let i = 0; i < numberOfDays; i++) {
         days.push(new DayLabel(currentDate));
         currentDate.add(1, 'day');
       }
     };
 
     // Calculate the number of days until the end of the month
-    const daysUntilEndOfMonth = moment(currentDate).endOf('month').date() - currentDate.date() + 1;
+  
 
     // Add labels for the current month, starting from the current day
-    addDays(daysUntilEndOfMonth, currentDate, days);
 
-    const remainingDaysFromNextMonth = Math.max(0, daysUntilEndOfMonth - currentDate.date());
+
 
     // Adjust the loop for days from the next month
-    addDays(remainingDaysFromNextMonth, currentDate, days);
+    addDays(30, currentDate, days);
 
     return days;
   }
