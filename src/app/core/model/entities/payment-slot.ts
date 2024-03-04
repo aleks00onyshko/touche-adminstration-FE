2778153fbcc4a2a2264dcabbb1b61043091cd844
asasin098676 <76844369+asasin098676@users.noreply.github.com>
2024-03-04 16:32:03 +0200

    attendeeId

--- src/app/core/model/entities/payment-slot.ts
+++ src/app/core/model/entities/payment-slot.ts
@@ -3,5 +3,5 @@ import { Entity } from "./entity";
 export interface PaymentSlot extends Entity<string> {
         numberOfClasses: number,
         price: number
-        attendeeId: string;
+      
 }
