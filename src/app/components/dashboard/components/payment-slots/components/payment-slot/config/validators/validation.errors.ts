9001609abb2ad084c6276c81cfc3b4129229d164
asasin098676 <76844369+asasin098676@users.noreply.github.com>
2024-03-05 17:45:37 +0200

    change payment slot validatorand change in edit

--- src/app/components/dashboard/components/payment-slots/components/payment-slot/config/validators/validation.errors.ts
+++ src/app/components/dashboard/components/payment-slots/components/payment-slot/config/validators/validation.errors.ts
@@ -1,11 +1,8 @@
 import { ValidationErrors } from '@angular/forms';
 
 export enum PaymentSlotCardValidationErrorsEnum {
-<<<<<<< HEAD
-  OVERLAPS_WITH_OTHER = 'OVERLAPS_WITH_OTHER'
-=======
-HAS_PAYMENT_TURNER_SYNDROME="HAS_PAYMENT_TURNER_SYNDROME"
->>>>>>> 5bc00b1 (new change)
+  OVERLAPS_WITH_OTHER = 'OVERLAPS_WITH_OTHER',
+  HAS_PAYMENT_TURNER_SYNDROME = 'HAS_PAYMENT_TURNER_SYNDROME'
 }
 
 export type PaymentSlotCardValidationErrors = ValidationErrors & {
